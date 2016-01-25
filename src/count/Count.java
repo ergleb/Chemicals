acacef594b9170aa51c0b14a7f7aa9de863a0a10
ergleb <ergleb23@gmail.com>
2016-01-25 03:28:55 +0200

    Made like Zaets wanted

--- src/count/Count.java
+++ src/count/Count.java
@@ -11,8 +11,9 @@ public class Count {
     double C;
     double q;
     double alpha;
+    double[] tMax;
 
-    final double eps = 1;
+    final double eps = 0.1;
     public Count(double p, double k, double c, double q, double alpha) {
         P = p;
         K = k;
@@ -21,48 +22,58 @@ public class Count {
         this.alpha = alpha;
     }
 
-    public void getLucky(){//превращает задачу в задачу оптимиста
+    public Count(double p, double k, double c, double q, double alpha, double[] tMax) {
+        P = p;
+        K = k;
+        C = c;
+        this.q = q;
+        this.alpha = alpha;
+        this.tMax = tMax;
+    }
+
+    public void getLucky(){//РїСЂРµРІСЂР°С‰Р°РµС‚ Р·Р°РґР°С‡Сѓ РІ Р·Р°РґР°С‡Сѓ РѕРїС‚РёРјРёСЃС‚Р°
         P = P * (1 - Math.sqrt(1 / alpha - 1));
         C = C * (1 - Math.sqrt(Math.log(Math.pow(alpha,-2))));
         q = q * (1 - Math.sqrt(2 * (1/alpha - 1)));
     }
 
-    public void getUnLucky(){//превращает в задачу пессимиста
+    public void getUnLucky(){//РїСЂРµРІСЂР°С‰Р°РµС‚ РІ Р·Р°РґР°С‡Сѓ РїРµСЃСЃРёРјРёСЃС‚Р°
         P = P * (1 + Math.sqrt(1 / alpha - 1));
         C = C * (1 + Math.sqrt(Math.log(Math.pow(alpha,-2))));
         q = q * (1 + Math.sqrt(2* (1/alpha - 1)));
     }
 
 
-    public double targetf (double[] n, double[] t){//подсчет целефой ф-ии
-        double ans = C * n[0] + 2 * n[0] * q * (t[0] * t[1] + t[0] * t[2] + t[1] * t[2]);
-        ans -= q * (n[1] * t[0] * t[1] + n[2] * t[0] * t[2] + n[3] * t[1] * t[2]);
+    public double targetf (double n, double n0, double[] t){//РїРѕРґСЃС‡РµС‚ С†РµР»РµС„РѕР№ С„-РёРё
+        double ans = C * n + 2 * n * q * (t[0] * t[1] + t[0] * t[2] + t[1] * t[2]);
+        ans -= q * (n0 * t[0] * t[1] + n0 * t[0] * t[2] + n0 * t[1] * t[2]);
         return ans;
     }
 
-    public double alphaf (double[] n, double[] t, double p){//подсчет штрафной ф-ии
-        double ans = Math.pow(Math.max(0,-(n[0]*t[0]*t[1]*t[2]-P)),p);
-        ans += Math.pow(Math.max(0,-(K - (n[1] * t[0] * t[1] + n[2] * t[0] * t[2] + n[3] * t[1] * t[2]))),p);
+    public double alphaf (double n, double n0, double[] t, double p){//РїРѕРґСЃС‡РµС‚ С€С‚СЂР°С„РЅРѕР№ С„-РёРё
+        double ans = Math.pow(Math.max(0,-(n*t[0]*t[1]*t[2]-P)),p);
+        ans += Math.pow(Math.max(0,-(K - (n0 * t[0] * t[1] + n0 * t[0] * t[2] + n0 * t[1] * t[2]))),p);
         ans += Math.pow(Math.max(0,-t[0]),p) + Math.pow(Math.max(0,-t[1]),p) + Math.pow(Math.max(0,-t[2]),p);
+        ans += Math.pow(Math.max(0,t[0]-tMax[0]),p) + Math.pow(Math.max(0,t[1]-tMax[1]),p) + Math.pow(Math.max(0,t[2]-tMax[2]),p);
         return ans;
     }
 
-    public double f(double[] n, double[] t, double r, double p){//подсчет общей функции
-        return targetf(n, t) + r * alphaf(n, t, p);
+    public double f(double n, double n0, double[] t, double r, double p){//РїРѕРґСЃС‡РµС‚ РѕР±С‰РµР№ С„СѓРЅРєС†РёРё
+        return targetf(n, n0, t) + r * alphaf(n, n0, t, p);
     }
 
-    public double[] gradf (double[] n, double[] t, double r, double p){//подсчет градиента
+    public double[] gradf (double n, double n0, double[] t, double r, double p){//РїРѕРґСЃС‡РµС‚ РіСЂР°РґРёРµРЅС‚Р°
         double[] ans = new double[3];
         double h = 0.001;
         for (int i = 0; i < 3; i++) {
             double[] tShift = Arrays.copyOf(t, 3);
             tShift[i] += h;
-            ans[i] = (f(n,tShift,r,p) - f(n,t,r,p))/h;
+            ans[i] = (f(n,n0,tShift,r,p) - f(n,n0,t,r,p))/h;
         }
         return ans;
     }
 
-    double norm (double[] x){//подсчет нормы вектора
+    double norm (double[] x){//РїРѕРґСЃС‡РµС‚ РЅРѕСЂРјС‹ РІРµРєС‚РѕСЂР°
         double ans = 0;
         for (double el : x){
             ans += Math.pow(el,2);
@@ -70,12 +81,12 @@ public class Count {
         return Math.sqrt(ans);
     }
 
-    public double[] gradDescent (double[] n, double[] t, double r, double p, double step){//градиентный спуск(минимизация функций)
+    public double[] gradDescent (double n, double n0, double[] t, double r, double p, double step){//РіСЂР°РґРёРµРЅС‚РЅС‹Р№ СЃРїСѓСЃРє(РјРёРЅРёРјРёР·Р°С†РёСЏ С„СѓРЅРєС†РёР№)
         int itn = 0;
         double[] tNew = Arrays.copyOf(t,t.length);
         double[] grad;
         do{
-            grad = gradf(n, tNew, r, p);
+            grad = gradf(n, n0, tNew, r, p);
             itn++;
             for (int i = 0; i < tNew.length; i++) {
                 tNew[i] -= step * grad[i];
@@ -86,49 +97,58 @@ public class Count {
         return tNew;
     }
 
-    public double[] solve (double[] n, double r1, double p, double beta){//метод штрафных функций
+    public double[] solve (double n, double n0, double r1, double p, double beta){//РјРµС‚РѕРґ С€С‚СЂР°С„РЅС‹С… С„СѓРЅРєС†РёР№
         double[] t = new double[3];
         t[1] = t[2] = t[0] = 1;
         int count = 0;
         while (count < 50){
             count++;
-            t = gradDescent(n,t,r1,p,0.000001);//тут 0.000001 - шаг градиентного спуска
-            double d = r1 * alphaf(n,t,p);
+            t = gradDescent(n,n0,t,r1,p,0.0000001);//С‚СѓС‚ 0.000001 - С€Р°Рі РіСЂР°РґРёРµРЅС‚РЅРѕРіРѕ СЃРїСѓСЃРєР°
+            double d = r1 * alphaf(n,n0,t,p);
             if (d<eps) return t;
             r1 *= beta;
         }
         return null;
     }
 
+    public double getP() {
+        return P;
+    }
+
     public static void main(String[] args) {
-        Count count = new Count(1000,20,10,3,1.0);//создаем объект с параметрами
-        //count.getLucky();//тут превращаем функцию в оптимиста/пессимиста, если нужно
-        double[] minn = {1,0,0,0};//выбираем начальное минимальное значение
-        double[] mint = count.solve(minn, 10, 1, 2);
-        double minf = count.targetf(minn, mint);
-        outer: for (int i = 1; i <= 3; i++) {//циклы для перебора целых чисел, тут можно поменять макс. количество контейнеров
+        Count count = new Count(1000,53,10,3,0.8,new double[]{6,4,3});//СЃРѕР·РґР°РµРј РѕР±СЉРµРєС‚ СЃ РїР°СЂР°РјРµС‚СЂР°РјРё
+        //count.getUnLucky();//С‚СѓС‚ РїСЂРµРІСЂР°С‰Р°РµРј С„СѓРЅРєС†РёСЋ РІ РѕРїС‚РёРјРёСЃС‚Р°/РїРµСЃСЃРёРјРёСЃС‚Р°, РµСЃР»Рё РЅСѓР¶РЅРѕ
+        double mincont = Math.ceil(count.P/(6*4*3));
+        double minn = 0;
+        double minn0 = 0;
+        double[] mint = null;
+        double minf = Double.MAX_VALUE;
+        outer: for (int i = (int)mincont; i <= (int)mincont+5; i++) {//С†РёРєР»С‹ РґР»СЏ РїРµСЂРµР±РѕСЂР° С†РµР»С‹С… С‡РёСЃРµР», С‚СѓС‚ РјРѕР¶РЅРѕ РїРѕРјРµРЅСЏС‚СЊ РјР°РєСЃ. РєРѕР»РёС‡РµСЃС‚РІРѕ РєРѕРЅС‚РµР№РЅРµСЂРѕРІ
             for (int j = 0; j <= i; j++) {
-                for (int k = 0; k <= j; k++) {
-                    for (int l = 0; l <= k; l++) {
-                        double[] n = {i,j,k,l};
-                        double[] t = count.solve(n, 10, 1, 2);
-                        if(t != null){//если ответ существует, то проверяем минимальность
-                            double f = count.targetf(n, t);
-                            if (f < minf){
-                                minf = f;
-                                minn = n;
-                                mint = t;
-                            }
-                        } else continue outer;
+                double[] t = count.solve(i,j,10,1,2);
+                System.out.println(i + " " + j);
+                if (t!=null) {
+                    System.out.println(Arrays.toString(t));
+                    System.out.println(count.targetf(i,j,t));
+                        if(count.targetf(i,j,t)<minf){
+                            minn = i;
+                            minn0 = j;
+                            mint = t;
+                            minf = count.targetf(i,j,t);
+                        }
                     }
-                }
+
+                else continue outer;
             }
         }
-        System.out.println("Number of boxes: " + minn[0]);//выводим на экран
-        System.out.println("Size of boxes: " + Arrays.toString(mint));
-        System.out.println("Cost: " + minf);
-
-
+        try {
+            System.out.println("РљРѕР»РёС‡РµСЃС‚РІРѕ РєРѕРЅС‚РµР№РЅРµСЂРѕРІ: " + minn);//РІС‹РІРѕРґРёРј РЅР° СЌРєСЂР°РЅ
+            System.out.println("Р Р°Р·РјРµСЂС‹: " + Arrays.toString(mint));
+            System.out.println("РЎС‚РѕРёРјРѕСЃС‚СЊ: " + minf);
+        }
+        catch (NullPointerException ex){
+            System.out.println("can't do it");
+        }
     }
 }
 
--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -15,12 +15,16 @@ public class Controller {
     double K;
     double q;
     double alpha;
+    double[] tMax;
 
     @FXML protected TextField PInput;
     @FXML protected TextField KInput;
     @FXML protected TextField CInput;
     @FXML protected TextField qInput;
     @FXML protected TextField alphaInput;
+    @FXML protected TextField t1Input;
+    @FXML protected TextField t2Input;
+    @FXML protected TextField t3Input;
     @FXML protected TextArea Output;
 
     @FXML
@@ -31,92 +35,92 @@ public class Controller {
         C = Double.parseDouble(CInput.getText());
         q = Double.parseDouble(qInput.getText());
         alpha = Double.parseDouble(alphaInput.getText());
-        Count count = new Count(P,K,C,q,alpha);//создаем объект с параметрами
+        tMax = new double[3];
+        tMax[0] = Double.parseDouble(t1Input.getText());
+        tMax[1] = Double.parseDouble(t2Input.getText());
+        tMax[2] = Double.parseDouble(t3Input.getText());
+        Count count = new Count(P,K,C,q,alpha,tMax);//создаем объект с параметрами
         //count.getLucky();//тут превращаем функцию в оптимиста/пессимиста, если нужно
-        double[] minn = {1,0,0,0};//выбираем начальное минимальное значение
-        double[] mint = count.solve(minn, 10, 1, 2);
-        double minf = count.targetf(minn, mint);
-        outer: for (int i = 1; i <= 1; i++) {//циклы для перебора целых чисел, тут можно поменять макс. количество контейнеров
+        double mincont = Math.ceil(count.getP()/(6*4*3));
+        double minn = 0;
+        double minn0 = 0;
+        double[] mint = null;
+        double minf = Double.MAX_VALUE;
+        outer: for (int i = (int)mincont; i <= (int)mincont+2; i++) {//циклы для перебора целых чисел, тут можно поменять макс. количество контейнеров
             for (int j = 0; j <= i; j++) {
-                for (int k = 0; k <= j; k++) {
-                    for (int l = 0; l <= k; l++) {
-                        double[] n = {i,j,k,l};
-                        double[] t = count.solve(n, 10, 1, 2);
-                        if(t != null){//если ответ существует, то проверяем минимальность
-                            double f = count.targetf(n, t);
-                            if (f < minf){
-                                minf = f;
-                                minn = n;
-                                mint = t;
-                            }
-                        } else continue outer;
+                double[] t = count.solve(i,j,10,1,2);
+                if (t!=null) {
+                    if(count.targetf(i,j,t)<minf){
+                        minn = i;
+                        minn0 = j;
+                        mint = t;
+                        minf = count.targetf(i,j,t);
                     }
                 }
+                else continue outer;
             }
         }
-
         magic.append("Четкая задача\n");
-        magic.append("Количество контейнеров: " + (int)minn[0] + "\n");//выводим на экран
+        magic.append("Количество контейнеров: " + minn + "\n");//выводим на экран
         magic.append("Размеры: " + Arrays.toString(mint) + "\n");
         magic.append("Стоимость: " + minf + "\n");
 
 
-        count = new Count(P,K,C,q,alpha);//создаем объект с параметрами
+        count = new Count(P,K,C,q,alpha,tMax);//создаем объект с параметрами
         count.getLucky();//тут превращаем функцию в оптимиста/пессимиста, если нужно
-        minn = new double[]{1,0,0,0};//выбираем начальное минимальное значение
-        mint = count.solve(minn, 10, 1, 2);
-        minf = count.targetf(minn, mint);
-        outer: for (int i = 1; i <= 1; i++) {//циклы для перебора целых чисел, тут можно поменять макс. количество контейнеров
+        mincont = Math.ceil(count.getP()/(6*4*3));
+        minn = 0;
+        minn0 = 0;
+        mint = null;
+        minf = Double.MAX_VALUE;
+        outer: for (int i = (int)mincont; i <= (int)mincont+2; i++) {//циклы для перебора целых чисел, тут можно поменять макс. количество контейнеров
             for (int j = 0; j <= i; j++) {
-                for (int k = 0; k <= j; k++) {
-                    for (int l = 0; l <= k; l++) {
-                        double[] n = {i,j,k,l};
-                        double[] t = count.solve(n, 10, 1, 2);
-                        if(t != null){//если ответ существует, то проверяем минимальность
-                            double f = count.targetf(n, t);
-                            if (f < minf){
-                                minf = f;
-                                minn = n;
-                                mint = t;
-                            }
-                        } else continue outer;
+                double[] t = count.solve(i,j,10,1,2);
+                System.out.println(i + " " + j);
+                if (t!=null) {
+                    System.out.println(Arrays.toString(t));
+                    System.out.println(count.targetf(i,j,t));
+                    if(count.targetf(i,j,t)<minf){
+                        minn = i;
+                        minn0 = j;
+                        mint = t;
+                        minf = count.targetf(i,j,t);
                     }
                 }
+                else continue outer;
             }
         }
 
         magic.append("Задача оптимиста:\n");
-        magic.append("Количество контейнеров: " + (int)minn[0] + "\n");//выводим на экран
+        magic.append("Количество контейнеров: " + minn+ "\n");//выводим на экран
         magic.append("Размеры: " + Arrays.toString(mint) + "\n");
         magic.append("Стоимость: " + minf + "\n");
 
 
-        count = new Count(P,K,C,q,alpha);//создаем объект с параметрами
+        count = new Count(P,K,C,q,alpha,tMax);//создаем объект с параметрами
         count.getUnLucky();//тут превращаем функцию в оптимиста/пессимиста, если нужно
-        minn = new double[]{1,0,0,0};//выбираем начальное минимальное значение
-        mint = count.solve(minn, 10, 1, 2);
-        minf = count.targetf(minn, mint);
-        outer: for (int i = 1; i <= 1; i++) {//циклы для перебора целых чисел, тут можно поменять макс. количество контейнеров
+        mincont = Math.ceil(count.getP()/(6*4*3));
+        minn = 0;
+        minn0 = 0;
+        mint = null;
+        minf = Double.MAX_VALUE;
+        outer: for (int i = (int)mincont; i <= (int)mincont+2; i++) {//циклы для перебора целых чисел, тут можно поменять макс. количество контейнеров
             for (int j = 0; j <= i; j++) {
-                for (int k = 0; k <= j; k++) {
-                    for (int l = 0; l <= k; l++) {
-                        double[] n = {i,j,k,l};
-                        double[] t = count.solve(n, 10, 1, 2);
-                        if(t != null){//если ответ существует, то проверяем минимальность
-                            double f = count.targetf(n, t);
-                            if (f < minf){
-                                minf = f;
-                                minn = n;
-                                mint = t;
-                            }
-                        } else continue outer;
+                double[] t = count.solve(i,j,10,1,2);
+                if (t!=null) {
+                    if(count.targetf(i,j,t)<minf){
+                        minn = i;
+                        minn0 = j;
+                        mint = t;
+                        minf = count.targetf(i,j,t);
                     }
                 }
+                else continue outer;
             }
         }
 
         magic.append("Задача пессимиста:\n");
-        magic.append("Количество контейнеров: " + (int)minn[0] + "\n");//выводим на экран
+        magic.append("Количество контейнеров: " + minn+ "\n");//выводим на экран
         magic.append("Размеры: " + Arrays.toString(mint) + "\n");
         magic.append("Стоимость: " + minf + "\n");
 
